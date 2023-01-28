b59e4205e776c969543c3dfba84f0c0d17f29517
brixmavu <mavubb@gmail.com>
2023-01-28 22:21:39 +0200

    Updated the UI

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+// Get the modal
+var modal = document.getElementsByClassName('modal');
+
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("modalBtn");
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal 
+btn[0].onclick = function () {
+    modal[0].style.display = "block";
+}
+
+btn[1].onclick = function () {
+    modal[1].style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span[0].onclick = function () {
+    modal[0].style.display = "none";
+}
+
+span[1].onclick = function () {
+    modal[1].style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal[0]) {
+        modal[0].style.display = "none";
+    }
+    if (event.target == modal[1]) {
+        modal[1].style.display = "none";
+    }
+}
+
+
 // Initialize variables for balance and transaction history
 let balance = 0;
 let transactionHistory = [];
@@ -61,4 +98,3 @@ document.getElementById("withdraw-funds-form").addEventListener("submit", functi
   let amount = document.getElementById("withdraw-amount").value;
   withdrawFunds(Number(amount));
 });
-
